221c617ba1ad65d6a922c267abeeb15fae9add39
Sebarosu <134329498+Sebarosu@users.noreply.github.com>
2026-08-03 13:55:45 +0200

    Add files via upload

--- español/main.lua
+++ español/main.lua
@@ -2,31 +2,35 @@ return function(mod)
 	-- Add Items
 	mod.content.items:patch("MIST_STONE", { name = "PIEDRA NIEBLA" })
 
-	SapuDex = "Un POKéMON grande\ncomo un edificio,\ncon un árbol\ntropical en el\nlomo que, en\nejemplares más\ngrandes, se\nvuelve un bosque\nentero."
+	SapuDex = "Un POKéMON grande\ncomo un edificio,\ncon un árbol en el\nlomo que, en los\nmás grandes, es un\bosque entero."
 	mod.content.text:patch("_SapusaurDexEntry", SapuDex)
 	mod.content.pokemon:patch("SAPUSAUR", {dexEntry = { kind = "MALEZA" }})
 
-	ColtDex = "Vive en el cora-\nzón de volcanes\nactivos que,\ncuando eruptan,\nhuye volando\na tal velocidad\nque nadie puede\npercibirlo con\nclaridad."
+	SkeloDex = "Esta evolución re-\nsulta de la inca-\npacidad del cuerpo\nde CHARIZARD a\naguantar su poder\ntras tantas heridas."
+	mod.content.text:patch("_SkelozardDexEntry", SkeloDex)
+	mod.content.pokemon:patch("SKELOZARD", {dexEntry = { kind = "ESQUELETO" }})
+
+	ColtDex = "Viven en el cora\nzón de volcanes\nactivos. Cuando\neruptan, huyen vo-\nlando tán rápido\nque no se percibe."
 	mod.content.text:patch("_CharcoltDexEntry", ColtDex)
 	mod.content.pokemon:patch("CHARCOLT", {dexEntry = { kind = "DRAGÓN FUEGO" }})
 
-	RainDex = "Se dice que este\nPOKéMON nace\ncuando a BLASTOISE\nle alcanza un rayo\nen plena tormenta.\nPuede destro-\nzar ciudades\nenteras con sus\ncañones."
+	RainDex = "Se dice que este POKéMON\nnace cuando a BLASTOISE\nle alcanza un rayo\nen plena tormenta. Puede\ndestrozar ciudades\nenteras con sus cañones."
 	mod.content.text:patch("_RainerDexEntry", RainDex)
 	mod.content.pokemon:patch("RAINER", {dexEntry = { kind = "TORMENTA" }})
 	
-	GoroDex = "GOROCHU es\ncompetitivo por\nnaturaleza.\nUsa sus dos\ngrandes colas,\ncargadas de elec-\ntricidad, para\natacar. Vive\n en zonas cálidas."
+	GoroDex = "GOROCHU es competitivo\npor naturaleza. Usa\nsus dos grandes colas,\ncargadas de elec-\ntricidad, para atacar."
 	mod.content.text:patch("_GorochuDexEntry", GoroDex)
 	mod.content.pokemon:patch("GOROCHU", {dexEntry = { kind = "RATÓN INTENSO" }})
 	
-	BluDex = "Se suele encontrar\nen los bosques y\noasis desérticos.\nPese a su piel\nrosada, su nombre\ntiene origen en\nel daltonismo."
+	BluDex = "Se suele encontrar\nen los bosques y\noasis desérticos.\nPese a su piel rosada,\nsu nombre proviene\ndel daltonismo."
 	mod.content.text:patch("_PikabluDexEntry", BluDex)
 	mod.content.pokemon:patch("PIKABLU", {dexEntry = { kind = "RATÓN ROSA" }})
 	
 	AzurDex = "Cuando juega en\nel agua, enrosca\nsus alargadas orejas\npara evitar que\nsus partes internas\nse mojen."
 	mod.content.text:patch("_AzuraiDexEntry", AzurDex)
-	mod.content.pokemon:patch("AZURAI", {dexEntry = { kind = "CONEJO LILA" }})
+	mod.content.pokemon:patch("AZURAI", {dexEntry = { kind = "CONEJOLILA" }})
 
-	JaggDex = "Patrulla por\naguas profundas\nguiado por su\ncaracterístico\nhocico. Puede\natravesar\nhasta cascos\nde navíos."
+	JaggDex = "Patrulla por\naguas profundas\nguiado por su\ncaracterístico\nhocico. Puede\natravesar\nhasta cascos de\nnavíos."
 	mod.content.text:patch("_JaggsDexEntry", JaggDex)
 	mod.content.pokemon:patch("JAGGS", {dexEntry = { kind = "TIBURÓN" }})
 	
@@ -37,16 +41,24 @@ return function(mod)
 	GyaDex = "Los rumores dicen\nque GYAOON surgió\nantes de que los POKéMON\ntuvieran tipos definidos.\nSu aspecto no ha\ncambiado en siglos."
 	mod.content.text:patch("_GyaoonDexEntry", GyaDex)
 	mod.content.pokemon:patch("GYAOON", {dexEntry = { kind = "BESTIA" }})
+
+	WhiteDex = "No se sabe si\nes un POKéMON\no un horror vivo.\nSolía ser una sim-\nple historia de miedo\nhasta su aparición."
+	mod.content.text:patch("_WhiteHandDexEntry", WhiteDex)
+	mod.content.pokemon:patch("WHITE_HAND", {dexEntry = { kind = "MALDITO" }})
+
+	BetaDex = "Se dice que surgió\nde un huevo en un\nlugar en el que no\nhabía nada. Y\nluego dio forma al\nnuestro mundo."
+	mod.content.text:patch("_BetarceusDexEntry", BetaDex)
+	mod.content.pokemon:patch("BETARCEUS", {dexEntry = { kind = "ALFA" }})
 	
-	YoshiDex = "Atrapa a sus\nenemigos con su\nlengua larga\ny los convierte\nen huevos, para\nluego lanzarlos\ncomo proyecti-\nles."
+	YoshiDex = "Atrapa a sus ene-\nmigos con su len-\ngua alargada y los\nconvierte en hue-\nvos, para tirarlos\ncomo proyectiles."
 	mod.content.text:patch("_YoshiDexEntry", YoshiDex)
 	mod.content.pokemon:patch("YOSHI", {dexEntry = { kind = "DINOSAURIO" }})
-	
-	KirbyDex = "Absorbe a sus\nenemigos inha-\nlando grandes\nbocanadas de\naire y copia\nsus mov. al\nInstante. Su\ncuerpo de globo\nle permite flo-\ntar sin fin."
-	mod.content.text:patch("_KirbyDexEntry", KirbyDex)
-	mod.content.pokemon:patch("KIRBY", {dexEntry = { kind = "ESTELAR" }})
 
-	SonicDex = "Es el ser vivo\nmás rápido de-\ntectado hasta\nel momento.\nPrefiere habitar\nprados verdes\ny detesta las\nmáquinas en\nsu totalidad."
+	SonicDex = "Es el ser vivo\nmás rápido de-\ntectado hasta\nel momento.\nPrefiere habitar\nprados verdes\ny detesta las\nmáquinas."
 	mod.content.text:patch("_SonicDexEntry", SonicDex)
 	mod.content.pokemon:patch("SONIC", {dexEntry = { kind = "ERIZO" }})
+
+	KirbyDex = "Inhala a sus riva-\nles y luego copia\nsus movimientos.\nSu cuerpo de globo\nle permite flotar\nindefinidamente."
+	mod.content.text:patch("_KirbyDexEntry", KirbyDex)
+	mod.content.pokemon:patch("KIRBY", {dexEntry = { kind = "ESTELAR" }})
 end
